4bea6dded33bdd6066fdfd4af151f9a0c1fe191a
Marco Wong <mckwxp@gmail.com>
2021-06-15 21:42:01 +0100

    Read countries.json
    - make countries.json into a single line
    - test reading json file in app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let myjson = require("./countries.json");
+  console.log(myjson);
   return (
     <div className="App">
       <header className="App-header">

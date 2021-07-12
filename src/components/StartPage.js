ec96377a66a2affcb0c9923c60702afb539fe38c
Marco Wong <mckwxp@gmail.com>
2021-07-12 14:59:54 +0100

    Fix flexbox layout bug and radio button form

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -18,38 +18,46 @@ function StartPage(props) {
             <form onSubmit={handleSubmit}>
                 Select the number of players:
                 <div>
-                    <input
-                        type="radio"
-                        id="numPlayers1"
-                        value={1}
-                        checked={props.players === 1}
-                        onChange={onValueChange}
-                    />
-                    <label>1</label>
-                    <input
-                        type="radio"
-                        id="numPlayers2"
-                        value={2}
-                        checked={props.players === 2}
-                        onChange={onValueChange}
-                    />
-                    <label>2</label>
-                    <input
-                        type="radio"
-                        id="numPlayers3"
-                        value={3}
-                        checked={props.players === 3}
-                        onChange={onValueChange}
-                    />
-                    <label>3</label>
-                    <input
-                        type="radio"
-                        id="numPlayers4"
-                        value={4}
-                        checked={props.players === 4}
-                        onChange={onValueChange}
-                    />
-                    <label>4</label>
+                    <label>
+                        <input
+                            type="radio"
+                            id="numPlayers1"
+                            value={1}
+                            checked={props.players === 1}
+                            onChange={onValueChange}
+                        />
+                        1
+                    </label>
+                    <label>
+                        <input
+                            type="radio"
+                            id="numPlayers2"
+                            value={2}
+                            checked={props.players === 2}
+                            onChange={onValueChange}
+                        />
+                        2
+                    </label>
+                    <label>
+                        <input
+                            type="radio"
+                            id="numPlayers3"
+                            value={3}
+                            checked={props.players === 3}
+                            onChange={onValueChange}
+                        />
+                        3
+                    </label>
+                    <label>
+                        <input
+                            type="radio"
+                            id="numPlayers4"
+                            value={4}
+                            checked={props.players === 4}
+                            onChange={onValueChange}
+                        />
+                        4
+                    </label>
                 </div>
                 <button type="submit">Play now!</button>
             </form>
